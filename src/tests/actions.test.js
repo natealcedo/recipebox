35aaa446a821a783d7a3ee644f0caa81579f76e9
Nate Alcedo <ndaljr@gmail.com>
2017-01-19 08:57:52 +0800

    update tests to use toggleRecipe actions

--- src/tests/actions.test.js
+++ src/tests/actions.test.js
@@ -12,6 +12,17 @@ describe('Action Creators', () => {
 		expect(actions.addRecipe('one egg', 'one egg, some egg white, one egg yolk')).toEqual(expectedAction);
 	});
 
+	it('should create an action to add a recipe and increment the id of the action', () => {
+		let expectedAction = {
+			type: actions.ADD_RECIPE,
+			recipe: 'porridge',
+			ingredients: ['egg', 'rice', 'shallots'],
+			id: 1
+		};
+
+		expect(actions.addRecipe('porridge', 'egg,rice,shallots')).toEqual(expectedAction);
+	});
+
 	it('should create an action to edit a current recipe', () => {
 		const expectedAction = {
 			type: actions.EDIT_RECIPE,
@@ -28,5 +39,14 @@ describe('Action Creators', () => {
 			id: 0
 		};
 		expect(actions.deleteRecipe(0)).toEqual(expectedAction);
+	});
+
+	it('should create an action to togle a recipe', () => {
+		let expectedAction = {
+			type: actions.TOGGLE_RECIPE,
+			id: 0
+		};
+
+		expect(actions.toggleRecipe(0)).toEqual(expectedAction);
 	});
 });
--- src/tests/reducers.test.js
+++ src/tests/reducers.test.js
@@ -1,93 +1,106 @@
-// import 'babel-polyfill';
 import * as reducers from '../reducers';
 
 describe('Reducers', () => {
 	it('should return the initial state', () => {
-		expect(reducers.recipes(undefined, 'random event'))
+		expect(reducers.recipes(undefined, 'UNDEFINED'))
 			.toEqual([]);
 	});
 
-	it('should be able to handle an addTodo action', () => {
+	it('should be able to handle an ADD_RECIPE action', () => {
 		let expectedState = [{
 			recipe: 'hotdog',
 			ingredients: ['bread', 'hotdog'],
-			id: 0
+			id: 0,
+			active: false
 		}];
 		expect(expectedState).toEqual(reducers.recipes([], {
 			type: 'ADD_RECIPE',
 			recipe: 'hotdog',
 			ingredients: ['bread', 'hotdog'],
-			id: 0
+			id: 0,
+			active: false
 		}));
 
 	});
 
-	it('should be able to handle an addTodo action when there is a current state', () => {
+	it('should be able to handle an ADD_RECIPE action when there is a current state', () => {
 		let initialState = [{
 			recipe: 'hotdog',
 			ingredients: ['bread', 'hotdog'],
-			id: 0
+			id: 0,
+			active: false
 		}, {
 			recipe: 'hotdog',
 			ingredients: ['bread', 'hotdog'],
-			id: 1
+			id: 1,
+			active: false
 		}];
 
 		let expectedState = [{
 			recipe: 'hotdog',
 			ingredients: ['bread', 'hotdog'],
-			id: 0
+			id: 0,
+			active: false
 		}, {
 			recipe: 'hotdog',
 			ingredients: ['bread', 'hotdog'],
-			id: 1
+			id: 1,
+			active: false
 		}, {
 			recipe: 'hotdog',
 			ingredients: ['bread', 'hotdog'],
-			id: 2
+			id: 2,
+			active: false
 		}];
 
 		expect(expectedState).toEqual(reducers.recipes(initialState, {
 			type: 'ADD_RECIPE',
 			recipe: 'hotdog',
 			ingredients: ['bread', 'hotdog'],
-			id: 2
+			id: 2,
+			active: false
 		}));
 	});
 
-	it('should be able to handle a deleteRecipe action', () => {
+	it('should be able to handle a DELETE_RECIPE action', () => {
 		let initialState = [{
 			recipe: 'hotdog',
 			ingredients: ['bread', 'hotdog'],
-			id: 0
+			id: 0,
+			active: false
 		},
 		{
 			recipe: 'hotdog',
 			ingredients: ['bread', 'hotdog'],
-			id: 1
+			id: 1,
+			active: false
 		},
 		{
 			recipe: 'hotdog',
 			ingredients: ['bread', 'hotdog'],
-			id: 2
+			id: 2,
+			active: false
 		}];
 
 		let expectedState = [{
 			recipe: 'hotdog',
 			ingredients: ['bread', 'hotdog'],
-			id: 0
+			id: 0,
+			active: false
 		}, {
 			recipe: 'hotdog',
 			ingredients: ['bread', 'hotdog'],
-			id: 2
+			id: 2,
+			active: false
 		}];
+
 		expect(reducers.recipes(initialState, {
 			type: 'DELETE_RECIPE',
 			id: 1
 		})).toEqual(expectedState);
 	});
 
-	it('should be able to handle an editRecipe action', () => {
+	it('should be able to handle an EDIT_RECIPE action', () => {
 		let initialState = [{
 			recipe: 'test',
 			ingredients: ['testing', 'testing'],
@@ -106,12 +119,12 @@ describe('Reducers', () => {
 		}]);
 	});
 
-	it('should be able to handle an editRecipe action with more than one recipe', () => {
+	it('should be able to handle an EDIT_RECIPE action with more than one recipe', () => {
 		let initialState = [{
 			recipe: 'test',
 			ingredients: ['testing', 'testing'],
 			id: 2
-		},{
+		}, {
 			recipe: 'test',
 			ingredients: ['testing', 'testing'],
 			id: 3
@@ -132,5 +145,119 @@ describe('Reducers', () => {
 			ingredients: ['testing', 'testing'],
 			id: 3
 		}]);
+	});
+
+	it('should be able to toggle a single recipe to active and inactive', () => {
+		let initialState = [{
+			recipe: 'egg',
+			ingredients: [],
+			id: 1,
+			active: false
+		}];
+		let expectedState = [{
+			recipe: 'egg',
+			ingredients: [],
+			id: 1,
+			active: true
+		}];
+
+		expect(reducers.recipes(initialState, { type: 'TOGGLE_RECIPE', id: 1 })).toEqual(expectedState);
+		expect(reducers.recipes(expectedState, { type: 'TOGGLE_RECIPE', id: 1 })).toEqual(initialState);
+
+	});
+
+	it('should only toggle one recipe when there there are many recipes', () => {
+		let initialState = [{
+			recipe: 'egg',
+			ingredients: [],
+			id: 0,
+			active: false
+		}, {
+			recipe: 'egg',
+			ingredients: [],
+			id: 1,
+			active: false
+		}, {
+			recipe: 'egg',
+			ingredients: [],
+			id: 2,
+			active: false
+		}];
+
+		let expectedState = [{
+			recipe: 'egg',
+			ingredients: [],
+			id: 0,
+			active: false
+		}, {
+			recipe: 'egg',
+			ingredients: [],
+			id: 1,
+			active: false
+		}, {
+			recipe: 'egg',
+			ingredients: [],
+			id: 2,
+			active: true
+		}];
+
+		let numberOfActiveRecipes =
+			reducers.recipes(initialState, { type: 'TOGGLE_RECIPE', id: 2 }).reduce((prev, curr) => {
+				if (curr.active) {
+					return prev + 1;
+				}
+				return prev;
+			}, 0);
+
+		expect(expectedState).toEqual(reducers.recipes(initialState, { type: 'TOGGLE_RECIPE', id: 2 }));
+		expect(numberOfActiveRecipes).toEqual(1);
+	});
+
+	it('should toggle an active recipe to false when another recipe is toggled', () => {
+		let initialState = [{
+			recipe: 'egg',
+			ingredients: [],
+			id: 0,
+			active: false
+		}, {
+			recipe: 'egg',
+			ingredients: [],
+			id: 1,
+			active: true
+		}, {
+			recipe: 'egg',
+			ingredients: [],
+			id: 2,
+			active: false
+		}];
+
+		let expectedState = [{
+			recipe: 'egg',
+			ingredients: [],
+			id: 0,
+			active: false
+		}, {
+			recipe: 'egg',
+			ingredients: [],
+			id: 1,
+			active: false
+		}, {
+			recipe: 'egg',
+			ingredients: [],
+			id: 2,
+			active: true
+		}];
+
+		let numberOfActiveRecipes =
+			reducers.recipes(initialState, { type: 'TOGGLE_RECIPE', id: 2 }).reduce((prev, curr) => {
+				if (curr.active) {
+					return prev + 1;
+				}
+				return prev;
+			}, 0);
+
+
+		expect(expectedState).toEqual(reducers.recipes(initialState, { type: 'TOGGLE_RECIPE', id: 2 }));
+		expect(numberOfActiveRecipes).toEqual(1);
 	});
 });
