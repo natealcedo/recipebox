c0165606b334017eedf32c648ad988e345ad83ea
Nate Alcedo <ndaljr@gmail.com>
2017-01-21 13:39:21 +0800

    Refactor form validation

--- src/components/RecipeEdit.js
+++ src/components/RecipeEdit.js
@@ -1,36 +1,39 @@
 import React from 'react';
 
 
-let RecipeEdit = ({onEditSubmit, onEditClick, id,recipe}) => {
+let RecipeEdit = ({onEditSubmit, onEditClick, id, recipe}) => {
 	let recipeInput;
 	let ingredients;
 
+	const validateForm = (e) => {
+		e.preventDefault();
+		if (!recipeInput.value) {
+			recipeInput.value = 'Value Required';
+			return;
+		}
+		if (!ingredients.value) {
+			ingredients.value = 'Value Required';
+			return;
+		}
+		if (!recipeInput.value || !ingredients.value) {
+			return;
+		}
+		onEditSubmit(recipeInput.value, ingredients.value.trim(), id);
+	};
 	return (
 		<div className='bg-primary form-group'>
 			<form>
 				<label>Recipe </label>
-				<input type="text" className='form-control'  defaultValue={recipe.recipe} ref={ref => recipeInput = ref} /><br />
+				<input type="text" className='form-control' defaultValue={recipe.recipe} ref={ref => recipeInput = ref} /><br />
 				<label>Ingredients </label>
-				<input type="text" className='form-control'  defaultValue={recipe.ingredients.join(',')}ref={ref => ingredients = ref} /><br />
+				<input type="text" className='form-control' defaultValue={recipe.ingredients.join(',')} ref={ref => ingredients = ref} /><br />
 				<button className='btn btn-info' type="submit" onClick={(e) => {
-					e.preventDefault();
-					if(!recipeInput.value){
-						recipeInput.value = 'Value Required';
-						return;
-					}
-					if(!ingredients.value){
-						ingredients.value = 'Value Required';
-						return;
-					}
-					if(!recipeInput.value || !ingredients.value){
-						return;
-					}
-					onEditSubmit(recipeInput.value, ingredients.value.trim(), id);
-				} } >Submit</button>
+					validateForm(e);
+				}} >Submit</button>
 				<button className='btn btn-danger' onClick={(e) => {
 					e.preventDefault();
 					onEditClick(id);
-				} }>Cancel</button>
+				}}>Cancel</button>
 			</form>
 		</div>
 	);
--- src/containers/AddRecipeModal.js
+++ src/containers/AddRecipeModal.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import {connect} from 'react-redux';
-import {addRecipe} from '../actions';
+import { connect } from 'react-redux';
+import { addRecipe } from '../actions';
+
+
 
 let AddRecipeModal = ({onClick}) => {
 	/**
@@ -10,47 +12,49 @@ let AddRecipeModal = ({onClick}) => {
 	let ingredients;
 	let recipeNotification;
 	let ingredientsNotification;
+
+	let validateForm = (e) => {
+		// Ensure form is not empty
+		e.preventDefault();
+		if (!recipe.value) {
+			recipeNotification.innerHTML = 'Value Required';
+		}
+		if (recipe.value) {
+			recipeNotification.innerHTML = ' ';
+		}
+		if (!ingredients.value) {
+			ingredientsNotification.innerHTML = 'Value Required';
+			return;
+		}
+		if (ingredients.value) {
+			ingredientsNotification.innerHTML = ' ';
+		}
+
+		if (!recipe.value || !ingredients.value) {
+			return;
+		}
+		onClick(recipe.value, ingredients.value.trim());
+		recipe.value = null;
+		ingredients.value = null;
+	};
 	return (
 		<div className='Modal'>
 			<form>
 				<label>Recipe</label>
-				<input className='form-control' type='text' label='Recipe' placeholder='Recipe Name' ref={ref=> {recipe = ref;}}/>
-				<label className='notification' ref={ref=> recipeNotification = ref }></label>
-				<br/>
+				<input className='form-control' type='text' label='Recipe' placeholder='Recipe Name' ref={ref => { recipe = ref; } } />
+				<label className='notification' ref={ref => recipeNotification = ref}></label>
+				<br />
 				<label>Ingredients</label>
-				<input className='form-control' type='textarea' label='Ingredients' placeholder='Enter Ingredients,Separated,By Commas' ref={ref => {ingredients = ref;}}/>
-				<label className='notification'ref={ref=> ingredientsNotification = ref }></label>
-				<br/>
-				<button type='submit' className='btn btn-primary' onClick={(e)=>{
-
-					// Ensure form is not empty
-					e.preventDefault();
-					if(!recipe.value){
-						recipeNotification.innerHTML = 'Value Required';
-					}
-					if(recipe.value){
-						recipeNotification.innerHTML = ' ';
-					}
-					if(!ingredients.value){
-						ingredientsNotification.innerHTML = 'Value Required';
-						return;
-					}
-					if(ingredients.value){
-						ingredientsNotification.innerHTML = ' ';
-					}
-
-					if(!recipe.value || !ingredients.value){
-						return;
-					}
-					onClick(recipe.value, ingredients.value.trim());
-					recipe.value = null;
-					ingredients.value = null;
-
+				<input className='form-control' type='textarea' label='Ingredients' placeholder='Enter Ingredients,Separated,By Commas' ref={ref => { ingredients = ref; } } />
+				<label className='notification' ref={ref => ingredientsNotification = ref}></label>
+				<br />
+				<button type='submit' className='btn btn-primary' onClick={(e) => {
+					validateForm(e);
 				}}>Submit</button>
-				<button onClick={(e)=>{
+				<button onClick={(e) => {
 					e.preventDefault();
 					document.getElementsByClassName('Modal')[0].classList.remove('show');
-				}}className='btn btn-danger'>Close</button>
+				} } className='btn btn-danger'>Close</button>
 			</form>
 		</div>
 	);
@@ -58,7 +62,7 @@ let AddRecipeModal = ({onClick}) => {
 
 const mapDispatchToProps = (dispatch) => {
 	return {
-		onClick: (recipe,ingredients) => {
+		onClick: (recipe, ingredients) => {
 			dispatch(addRecipe(recipe, ingredients));
 		}
 	};
