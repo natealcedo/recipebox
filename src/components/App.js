de5a1e7cbf7aac21f26d017acf63b19fa8399380
Nathaniel D Alcedo Jr <nathanielalcedojr@gmail.com>
2017-03-07 11:17:23 +0800

    remove eslint

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import AddRecipe from '../containers/AddRecipe';
-import AddRecipeModal from '../containers/AddRecipeModal';
-import ListOfRecipes from '../containers/ListOfRecipes';
-import '../css/App.css';
+import React from "react";
+import AddRecipe from "../containers/AddRecipe";
+import AddRecipeModal from "../containers/AddRecipeModal";
+import ListOfRecipes from "../containers/ListOfRecipes";
+import "../css/App.css";
 
 const App = () => {
-	return (
-		<div className='App'>
-			<AddRecipeModal></AddRecipeModal>
-			<AddRecipe></AddRecipe>
-			<ListOfRecipes></ListOfRecipes>
-		</div>
-	);
+  return (
+    <div className='App'>
+      <AddRecipeModal></AddRecipeModal>
+      <AddRecipe></AddRecipe>
+      <ListOfRecipes></ListOfRecipes>
+    </div>
+  );
 };
 export default App;
